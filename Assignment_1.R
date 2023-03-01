9680c5d3819b9b27832295680121471d208b9094
Christophe <70860455+cheukhin1024@users.noreply.github.com>
2023-03-01 15:17:17 +0800

    Update Assignment_1.R

--- Assignment_1.R
+++ Assignment_1.R
@@ -11,3 +11,15 @@ library(readxl)
 
 dat <- read_csv("C:/Users/s1224442/Downloads/Forbes Richest Atheletes (1990-2020).csv")
 describe(dat[,1:5])
+
+dat <- dat %>% select(name, nationality, sport, earnings, current_rank)
+
+## boxplot by group
+dat %>% select(name, nationality, sport, 
+               earnings, current_rank) %>%
+  reshape2::melt(id="name") %>%
+  ggplot(aes(x=variable, y=value, fill=variable))  + 
+  geom_boxplot() +
+  labs(x="nationality", y="earnings", 
+       title="Earnings in different countries") +
+  theme(plot.title=element_text(hjust=1.0))
