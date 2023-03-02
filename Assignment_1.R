99438a9a4e4f2ce3f8c7412409ccf333444b768d
Christophe <70860455+cheukhin1024@users.noreply.github.com>
2023-03-02 18:56:38 +0800

    Update Assignment_1.R

--- Assignment_1.R
+++ Assignment_1.R
@@ -1,3 +1,4 @@
+#Sys.setenv(LANG = "en")
 library(dplyr)
 library(ggplot2)
 library(psych)
@@ -8,18 +9,45 @@ library(car)
 library(knitr)
 library(gmodels)
 library(readxl)
+library(readr)
+library(reshape2)
+library(tidyverse)
 
-dat <- read_csv("C:/Users/s1224442/Downloads/Forbes Richest Atheletes (1990-2020).csv")
+### Descriptive statistics
+
+dat <- read_csv("C:/Users/Administrator/Downloads/Forbes Richest Atheletes (1990-2020).csv")
 describe(dat[,1:5])
 
 dat <- dat %>% select(name, nationality, sport, earnings, current_rank)
 
-## boxplot by group
+## First boxplot by group to study the relationship between 'nationality' and 'earnings'
 dat %>% select(name, nationality, sport, 
                earnings, current_rank) %>%
-  reshape2::melt(id="name") %>%
-  ggplot(aes(x=variable, y=value, fill=variable))  + 
-  geom_boxplot() +
+  ggplot(data = dat, mapping = aes(x = nationality, y = earnings))+ 
+  geom_point() + geom_smooth(method="gam") +
+  scale_x_discrete(guide = guide_axis(n.dodge=3))
   labs(x="Characteristics", y="Value", 
-       title="Earnings in different countries") +
+       title="Earnings in different nationalities") +
   theme(plot.title=element_text(hjust=0.5))
+
+## Second boxplot by group to study the relationship between 'sport' and 'earnings'
+dat %>% select(name, nationality, sport, 
+                earnings, current_rank) %>%
+  ggplot(data = dat, mapping = aes(x = sport, y = earnings))+ 
+  geom_point() + geom_smooth(method="gam") +
+  scale_x_discrete(guide = guide_axis(n.dodge=3))
+  labs(x="Characteristics", y="Value", 
+       title="Earnings in different sports") +
+  theme(plot.title=element_text(hjust=0.5))
+  
+## Second boxplot by group to study the relationship between 'sport' and 'earnings'
+dat %>% select(name, nationality, sport, 
+                earnings, current_rank) %>%
+  ggplot(data = dat, mapping = aes(x = sport, y = earnings))+ 
+  geom_point() + geom_smooth(method="gam") +
+  scale_x_discrete(guide = guide_axis(n.dodge=3))
+  labs(x="Characteristics", y="Value", 
+       title="Earnings in current rankings") +
+  theme(plot.title=element_text(hjust=0.5))
+  
+### Missing data analysis
